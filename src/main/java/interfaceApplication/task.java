1893e281f5f991dc6a90d4624619c170615ca977
GrapeSoftCN <jk282@163.com>
2017-10-12 19:44:53 +0800

    添加定时监控任务

--- src/main/java/interfaceApplication/task.java
+++ src/main/java/interfaceApplication/task.java
@@ -1,21 +1,35 @@
 package interfaceApplication;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.bson.types.ObjectId;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import JGrapeSystem.jGrapeFW_Message;
+import JGrapeSystem.rMsg;
 import apps.appsProxy;
 import authority.privilige;
+import cache.CacheHelper;
+import check.checkHelper;
 import database.DBHelper;
 import database.db;
+import email.mail;
 import httpClient.request;
+import interrupt.interrupt;
 import nlogger.nlogger;
 import rpc.execRequest;
+import security.codec;
 import session.session;
+import sms.ruoyaMASDB;
 import string.StringHelper;
 import time.TimeHelper;
 
 public class task {
+	private CacheHelper caches = new CacheHelper();
+	private DBHelper tasks = new DBHelper("mongodb", "task");
 	private static String nodeString = "mongodb";
 	private DBHelper dbHelper;
 	private String appid = appsProxy.appidString();
@@ -31,10 +45,257 @@ public class task {
 			// userInfo = (JSONObject) se.get(sid);
 			userInfo = se.getSession(sid);
 		}
+		searchAll(null);
 	}
 
-	private db bind(DBHelper tasks) {
+	private db bind() {
 		return tasks.bind(appid);
+	}
+
+	private db bind(DBHelper helper) {
+		return helper.bind(appid);
+	}
+
+	/**
+	 * 新增任务信息
+	 * 
+	 * @project GrapeTask
+	 * @package interfaceApplication
+	 * @file task.java
+	 * 
+	 * @param taskInfo
+	 *            ,包含Api地址需进行base64编码+特殊格式编码
+	 *
+	 */
+	public String AddTask(String taskInfo) {
+		int code = 0;
+		taskInfo = CheckParam(taskInfo);
+		JSONObject object = JSONObject.toJSON(taskInfo);
+		if (object.containsKey("errorcode")) {
+			return taskInfo;
+		}
+		code = bind().data(object).insertOnce() != null ? 0 : 99;
+		if (code == 0) {
+			caches.delete("APICheck_" + appid);
+			searchAll(null);
+		}
+		return resultMessage(code, "新增任务成功");
+	}
+
+	/**
+	 * 修改任务信息
+	 * 
+	 * @project GrapeTask
+	 * @package interfaceApplication
+	 * @file task.java
+	 * 
+	 * @param taskInfo
+	 *            ,包含Api地址需进行base64编码+特殊格式编码
+	 *
+	 */
+	public String UpdateTask(String tid, String taskInfo) {
+		int code = 99;
+		int openstate;
+		taskInfo = CheckParam(taskInfo);
+		JSONObject object = JSONObject.toJSON(taskInfo);
+		if (object != null && object.size() > 0) {
+			if (object.containsKey("errorcode")) {
+				return taskInfo;
+			}
+			code = bind().eq("_id", tid).data(object).update() != null ? 0 : 99;
+			if (code == 0) {
+				caches.delete("APICheck_" + appid);
+				searchAll(null);
+			}
+			if (object.containsKey("state")) {
+				String state = object.getString("state");
+				openstate = state.equals("1") ? 1 : 0;
+				String temp = ExcuteTask(0, openstate);
+				System.out.println(temp);
+			}
+		}
+		return resultMessage(code, "任务信息修改成功");
+	}
+
+	/**
+	 * 参数验证
+	 * 
+	 * @project GrapeTask
+	 * @package interfaceApplication
+	 * @file task.java
+	 * 
+	 * @param taskInfo
+	 * @return
+	 *
+	 */
+	@SuppressWarnings("unchecked")
+	private String CheckParam(String taskInfo) {
+		String temp = "";
+		int code = 0;
+		JSONObject object = JSONObject.toJSON(taskInfo);
+		if (object != null && object.size() > 0) {
+			if (object.containsKey("phone")) {
+				temp = object.getString("phone");
+				if (!temp.equals("")) {
+					code = checkHelper.checkMobileNumber(temp) ? 0 : 1;
+					if (code != 0) {
+						return resultMessage(code);
+					}
+				}
+			}
+			if (object.containsKey("email")) {
+				temp = object.getString("email");
+				if (!temp.equals("")) {
+					code = checkHelper.checkEmail(temp) ? 0 : 2;
+					if (code != 0) {
+						return resultMessage(code);
+					}
+				}
+			}
+			if (object.containsKey("taskContent")) {
+				temp = object.getString("taskContent");
+				if (!temp.equals("")) {
+					temp = codec.DecodeHtmlTag(temp);
+					temp = codec.decodebase64(temp);
+					object.put("taskContent", temp);
+				}
+			}
+		}
+		return (object != null && object.size() > 0) ? object.toString() : null;
+	}
+
+	/**
+	 * 执行全部处于开启状态的任务，每30秒执行一次任务
+	 * 
+	 * @project GrapeTask
+	 * @package interfaceApplication
+	 * @file task.java
+	 * 
+	 * @param hostid
+	 *            邮箱id
+	 *
+	 * @param state
+	 *
+	 */
+	public String ExcuteTask(int emailid, int state) {
+		String result = (String) appsProxy.proxyCall("/GrapeHeart/Heart/check/int:" + emailid + "/int:" + state, null, null);
+		return result;
+	}
+
+	public String DeleteTask(String id) {
+		int code = 99;
+		String[] ids = null;
+		db db = bind();
+		if (id != null && !id.equals("") && !id.equals("null")) {
+			ids = id.split(",");
+			if (ids != null && ids.length > 0) {
+				db.or();
+				for (String string : ids) {
+					if (ObjectId.isValid(string)) {
+						db.eq("_id", string);
+					}
+				}
+				code = db.deleteAll() == ids.length ? 0 : 99;
+			}
+		}
+		return rMsg.netMSG(code, code == 0 ? "删除成功" : "删除失败");
+	}
+
+	public String PageTask(int idx, int pageSize, String condString) {
+		searchAll(null);
+		ExcuteTask(0, 1);
+		db db = bind();
+		JSONArray array = null;
+		long total = 0, totalSize = 0;
+		if (condString != null && !condString.equals("") && !condString.equals("null")) {
+			JSONArray condArray = JSONArray.toJSONArray(condString);
+			if (condArray != null && condArray.size() > 0) {
+				db.where(condArray);
+			} else {
+				array = null;
+			}
+		}
+		array = db.dirty().page(idx, pageSize);
+		totalSize = db.pageMax(pageSize);
+		total = db.count();
+		return PageShow(array, total, totalSize, idx, pageSize);
+	}
+
+	// 分页显示部分
+	@SuppressWarnings("unchecked")
+	private String PageShow(JSONArray array, long total, long totalSize, int current, int PageSize) {
+		JSONObject object = new JSONObject();
+		object.put("data", (array != null && array.size() != 0) ? array : new JSONArray());
+		object.put("total", total);
+		object.put("totalSize", totalSize);
+		object.put("currentPage", current);
+		object.put("PageSize", PageSize);
+		return resultMessage(object);
+	}
+
+	// 获取所有的任务信息
+	@SuppressWarnings("unchecked")
+	private JSONObject searchAll(String authcode) {
+		JSONObject object = new JSONObject();
+		if (caches.get("APICheck_" + appid) != null) {
+			object = JSONObject.toJSON(caches.get("APICheck_" + appid));
+		} else {
+			JSONArray array = bind().eq("state", 0).select();
+			JSONObject emailObj = joinObj(array, "email");
+			JSONObject phoneObj = joinObj(array, "phone");
+			object.put("email", emailObj);
+			object.put("phone", phoneObj);
+			caches.setget("APICheck_" + appid, object, 60 * 60 * 12);
+		}
+		return object;
+	}
+
+	@SuppressWarnings("unchecked")
+	private JSONObject joinObj(JSONArray array, String filed) {
+		String key, tempkey, value = "", temp;
+		JSONObject object, Obj = new JSONObject();
+		if (array != null && array.size() > 0) {
+			for (Object object2 : array) {
+				object = (JSONObject) object2;
+				tempkey = object.getString(filed);
+				temp = object.getString("host");
+				if (Obj != null && Obj.size() > 0) {
+					for (Object obj : Obj.keySet()) {
+						key = obj.toString();
+						value = Obj.getString(key);
+						if (key.equals(tempkey)) {
+							value += "," + temp;
+						} else {
+							value = temp;
+						}
+					}
+					Obj.put(tempkey, StringHelper.fixString(value, ','));
+				} else {
+					Obj.put(tempkey, temp);
+				}
+			}
+		}
+		return Obj;
+	}
+
+	/**
+	 * 获取appid，类名称，方法名称，参数
+	 * 
+	 * @project GrapeTask
+	 * @package interfaceApplication
+	 * @file task.java
+	 * 
+	 * @param str
+	 * @return appid.class.func.param1.param2.param3
+	 *
+	 */
+	public String getFunc(String str) {
+		String[] strs = str.split("/");
+		String result = strs[3] + "." + strs[4] + "." + strs[5] + "." + strs[6];
+		for (int i = 7; i < strs.length; i++) {
+			result += "." + strs[i];
+		}
+		return result;
 	}
 
 	/**
@@ -133,16 +394,15 @@ public class task {
 		return RestTime(array);
 	}
 
-	//获取最新咨询及举报
+	// 获取最新咨询及举报
 	@SuppressWarnings("unchecked")
 	public JSONArray GetLetter(String sid) {
 		JSONObject obj;
 		String id;
 		JSONObject users = se.getSession(sid);
-		id = (users != null && users.size() != 0)
-				? ((JSONObject) users.get("_id")).getString("$oid") : " ";
-		db Sugdb = new DBHelper("mongodb","suggest").bind(appsProxy.appidString());
-		db Repdb = new DBHelper("mongodb","report").bind(appsProxy.appidString());
+		id = (users != null && users.size() != 0) ? ((JSONObject) users.get("_id")).getString("$oid") : " ";
+		db Sugdb = new DBHelper("mongodb", "suggest").bind(appsProxy.appidString());
+		db Repdb = new DBHelper("mongodb", "report").bind(appsProxy.appidString());
 		JSONArray SuggestArray = Sugdb.desc("time").eq("userid", id).limit(50).select();
 		JSONArray ReportArray = Repdb.desc("time").eq("userid", id).limit(50).select();
 		for (Object object : ReportArray) {
@@ -151,8 +411,7 @@ public class task {
 		}
 		return SuggestArray;
 	}
-	
-	
+
 	/**
 	 * 获取剩余更新时间
 	 * 
@@ -217,8 +476,7 @@ public class task {
 		temp = object.getString("lastTime");
 		tempTimediff = object.getString("timediff");
 		editTemp = object.getString("editCount");
-		if (temp != null && !temp.equals("") && tempTimediff != null && !tempTimediff.equals("") && editTemp != null
-				&& !editTemp.equals("")) {
+		if (temp != null && !temp.equals("") && tempTimediff != null && !tempTimediff.equals("") && editTemp != null && !editTemp.equals("")) {
 			if (temp.contains("$numberLong")) {
 				temp = JSONObject.toJSON(temp).getString("$numberLong");
 			}
@@ -358,11 +616,21 @@ public class task {
 		return resultMessage(0, _obj.toString());
 	}
 
+	private String resultMessage(int num) {
+		return resultMessage(num, "");
+	}
+
 	private String resultMessage(int num, String message) {
 		String msg = "";
 		switch (num) {
 		case 0:
 			msg = message;
+			break;
+		case 1:
+			msg = "手机号格式错误，请检查并修正";
+			break;
+		case 2:
+			msg = "邮箱格式错误，请检查并修正";
 			break;
 		}
 		return jGrapeFW_Message.netMSG(num, msg);
@@ -390,7 +658,7 @@ public class task {
 		// }
 		// JSONArray columns = JSONArray.toJSONArray(temp);
 		JSONArray columns = getColumnByType();
-		nlogger.logout("columns : "+columns);
+		nlogger.logout("columns : " + columns);
 		if (columns != null && columns.size() != 0) {
 			int l = columns.size();
 			if (l > 0) {
@@ -459,7 +727,7 @@ public class task {
 			db.eq("wbid", wbid);
 		}
 		db.eq("contentType", "5").field("_id,name").limit(50);
-		nlogger.logout("condString: "+db.condString());
+		nlogger.logout("condString: " + db.condString());
 		JSONArray array = db.select();
 		db.clear();
 		return array;
